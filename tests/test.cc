5895dc2c54715c855321692b35c0dfba176ad389
keshuigu <keshuigu_yl@qq.com>
2025-04-22 16:56:53 +0800

    add example test

---
+++ tests/test.cc
@@ -0,0 +1,12 @@
+
+#include <algorithm>
+#include <random>
+#include <vector>
+
+#include "gtest/gtest.h"
+
+namespace run_record {
+
+TEST(Example, ExampleTest) { ASSERT_TRUE(true); }
+
+}  // namespace run_record
